e18a3f035b80f466bde3e19a897fbf633c2147d5
jared-bloomer <me@jaredbloomer.com>
2021-07-26 17:21:49 +0100

    Adding missing flag

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt update && sudo apt install ansible python-pymysql python3-pymysql -y
