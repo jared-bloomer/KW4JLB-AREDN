e5add6b91e22da0a3b2645257db3efee725a5e7f
jared-bloomer <me@jaredbloomer.com>
2021-07-27 15:15:26 +0100

    Cleaning up Branch

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt install software-properties-common -y
-echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt update && sudo apt install ansible python-pymysql python3-pymysql -y
-ansible-galaxy collection install community.mysql
-
--- scripts/deletemailbox.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-read -p "Enter the username of the Mailbox to delete: " MBOXNAME
-
-sudo userdel $MBOXNAME
-
-exit 0
