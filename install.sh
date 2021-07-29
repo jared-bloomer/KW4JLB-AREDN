401c5b711a75ad8faa058ab004820cc1b8934f7b
jared-bloomer <me@jaredbloomer.com>
2021-07-29 01:32:48 +0100

    Adding additional ansible modules

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo te
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt update && sudo apt install ansible python-pymysql python3-pymysql -y
 ansible-galaxy collection install community.mysql
+ansible-galaxy collection install community.general
 
